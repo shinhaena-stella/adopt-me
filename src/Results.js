24df165354cac515c2086f56ff4b65c65119a056
shinhaena-stella <shinhaena@gmail.com>
2022-07-02 13:13:50 -0700

    working on Results.js to add sorting function

--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import Pet from "./Pet";
 
 const Results = ({ pets }) => {
+  // const [petsList, setPetsList] = useState({pets});
+
+  const sortByName = () => {
+    if (pets.length) {
+      let newList = [...pets];
+      newList.sort((a, b) => (b.name - a.name));
+      setPetsList(newList);
+    }
+  }
+  const sortByState = () => {
+
+  }
+
   return (
+    <>
+    <div className="buttons">
+      <button onClick={sortByName}>Sort by Name</button>
+      <button onClick={sortByState}>Sort by State</button>
+    </div>
     <div className="search">
       {!pets.length ? (
         <h1>No Pets Found</h1>
@@ -21,6 +40,7 @@ const Results = ({ pets }) => {
         })
       )}
     </div>
+    </>
   );
 };
 
